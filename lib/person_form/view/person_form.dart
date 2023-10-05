d7623ac000c540acb7a9d9e4612c750fbc6bf6ce
Felix Wielander <felix.wielander@netscrapers.com>
2023-10-05 16:24:24 +0200

    Build simple 1 person form

--- lib/person_form/view/person_form.dart
+++ lib/person_form/view/person_form.dart
@@ -18,14 +18,21 @@ class PersonForm extends StatelessWidget {
       listener: (context, state) {
         onChanged(state);
       },
-      child: const Column(
-        children: [
-          NameField(),
-          SizedBox(height: 8.0),
-          AgeField(),
-          SizedBox(height: 8.0),
-          RoleDropdownButton(),
-        ],
+      child: const Card(
+        margin: EdgeInsets.all(16.0),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              NameField(),
+              SizedBox(height: 8.0),
+              AgeField(),
+              SizedBox(height: 8.0),
+              RoleDropdownButton(),
+            ],
+          ),
+        ),
       ),
     );
   }
